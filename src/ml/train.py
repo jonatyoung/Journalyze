93c58566b5ab23bef476113047cc0defc125916a
federico <federicoberto708@gmail.com>
2025-03-15 16:31:20 +0700

    refactor(training) : change directory path to dynamic

--- src/ml/train.py
+++ src/ml/train.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
 import pickle
+from pathlib import Path
 from bertopic import BERTopic
 import umap
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 # 1. Load Data
-df = pd.read_csv("/home/jonathan/workdir/project/mlops/topic_modelling/data/preprocessed/preprocessed_data.csv")  # Ganti dengan nama file yang sesuai
+DATA_DIR = BASE_DIR / "data"
+MODEL_DIR = BASE_DIR / "models"
+
+df = pd.read_csv(DATA_DIR / "preprocessed/preprocessed_data.csv")  # Ganti dengan nama file yang sesuai
 texts = df['cleaned_judul'].tolist()  # Ambil teks judul bersih
-embeddings = np.load("/home/jonathan/workdir/project/mlops/topic_modelling/data/bert_embeddings.npy")  # Load embeddings dari BERT
+embeddings = np.load(DATA_DIR / "bert_embeddings.npy")  # Load embeddings dari BERT
 
 # 2. UMAP untuk Dimensionality Reduction (Opsional, mempercepat pemrosesan)
 umap_model = umap.UMAP(n_neighbors=15, n_components=5, metric='cosine', random_state=42)
@@ -25,9 +32,9 @@ topic_model.visualize_topics()  # Representasi topik di 2D space
 
 
 # 7. Simpan Model dalam Format Pickle
-with open("/home/jonathan/workdir/project/mlops/topic_modelling/models/bertopic_model.pkl", "wb") as file:
+with open(MODEL_DIR / "bertopic_model.pkl", "wb") as file:
     pickle.dump(topic_model, file)
 
 # 8. (Opsional) Menyimpan Hasil Topik ke CSV
 df['topic'] = topics
-df.to_csv("/home/jonathan/workdir/project/mlops/topic_modelling/data/hasil_topik.csv", index=False)
+df.to_csv(DATA_DIR / "hasil_topik.csv", index=False)
